2d9d502f51992d49f0fb40210aef41fdc5f4d903
veronloz <veronica.lozadap@gmail.com>
2025-03-18 19:39:50 +0100

    Añade consultas del ejercicio 3

--- scripts/consultas.js
+++ scripts/consultas.js
@@ -80,3 +80,121 @@ db.restaurants.find({rating : {$gt: 4}})
 /*
 EX 3
 */
+
+// Verificar el rating máximo no nulo
+use("restaurant_db");
+db.restaurants.aggregate([
+    {
+        $match: {
+            rating: { $nin: [null, "", "Not yet rated"] }
+        }
+    },
+    {
+        $group: {
+            _id: null,
+            maxField: { $max: "$rating" }
+        }
+    }
+])
+
+// Agrupar restaurantes por tipo de comida y calificación promedio
+use("restaurant_db");
+db.restaurants.aggregate([
+    {
+        $match: {
+            rating: { $nin: [null, "", "Not yet rated"] }
+        }
+    },
+    {
+        $group: {
+            _id: "$type_of_food",
+            number_of_restaurants: { $sum: 1 },
+            average_rating: { $avg: "$rating" }
+        }
+    }
+])
+
+// Número de inspecciones por resultado y porcentajes
+use("restaurant_db");
+db.inspections.aggregate([
+    {
+        $group: {
+            _id: "$result",
+            inspections_by_result: { $sum: 1 }
+        },
+    },
+    {
+        $group: {
+            _id: null,
+            total_inspections: { $sum: "$inspections_by_result" },
+            result: { $push: { name: "$_id", inspections_by_result: "$inspections_by_result" } }
+        }
+    },
+    {
+        $unwind: "$result"
+    },
+    {
+        $project: {
+            _id: 0,
+            name: "$result.name",
+            inspections_by_result: "$result.inspections_by_result",
+            percentage: { $multiply: [{ $divide: ["$result.inspections_by_result", "$total_inspections"] }, 100] }
+        }
+    }
+])
+
+//Lookup originl para unir restaurantes con inspecciones
+use("restaurant_db");
+db.restaurants.aggregate([
+    {
+        "$lookup": {
+            "from": "inspections",
+            "localField": "_id",
+            "foreignField": "restaurant_id",
+            "as": "inspection_history"
+        }
+    }
+]) // devuele una arra vacío. Hay un problema con el match de los campos
+
+// Verificar el tipo de dato de _id
+use("restaurant_db");
+db.restaurants.aggregate([
+    {
+        $project: {
+            _id: 1,
+            type: { $type: "$_id" } // devuelve "objectId"
+        }
+    }
+])
+
+// Verificar el tipo de dato de restaurant_id
+use("restaurant_db");
+db.inspections.aggregate([
+    {
+        $project: {
+            restaurant_id: 1,
+            type: { $type: "$restaurant_id" } // devuelve "string"
+        }
+    }
+])
+
+// Lookup con restaurant_id como string
+use("restaurant_db");
+db.restaurants.aggregate([
+    {
+        $addFields: {
+            //toString porque el foreignField es un string
+            restaurant_id: { $toString: "$_id" }
+        }
+
+    },
+    {
+        "$lookup": {
+            "from": "inspections",
+            "localField": "restaurant_id",
+            "foreignField": "restaurant_id",
+            "as": "inspection_history"
+        }
+    },
+    { $out: "restaurants_inspections" } // guarda el resultado en una nueva collection
+])
