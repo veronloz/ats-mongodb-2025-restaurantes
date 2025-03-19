4ab00846654af9624394f3079fad343df3f9ad45
Xinyu Yu <solyu041127@gmail.com>
2025-03-19 23:59:16 +0100

    Cambios en consultas.js

--- scripts/consultas.js
+++ scripts/consultas.js
@@ -29,7 +29,47 @@ db.restaurants.distinct("rating")
 
 
 //JSON SQUEMA
-
+db.createCollection("inspections", {
+validator: {
+    $jsonSchema: {
+    bsonType: "object",
+    required: ["restaurant_id", "date", "result"],
+    properties: {
+        restaurant_id: {
+        bsonType: "string",
+        description: "Debe ser un string y es requerido para asociar la inspección a un restaurante."
+        },
+        date: {
+        bsonType: "date",
+        description: "Debe ser una fecha y es requerido para determinar la validez de la inspección."
+        },
+        result: {
+        bsonType: "string",
+        enum: ["Fail", "No Violation Issued", "Pass", "Violation Issued", "Warning Issued"],
+        description: "Debe ser uno de los valores predefinidos y es requerido."
+        }
+    }
+    }
+}
+});
+  
+db.createCollection("restaurants", {
+validator: {
+    $jsonSchema: {
+    bsonType: "object",
+    properties: {
+        rating: {
+        bsonType: ["number", "null"],
+        minimum: 1,
+        maximum: 10,
+        description: "Debe ser un número entre 1 y 10, o null si no está calificado aún."
+        }
+    }
+    }
+}
+});
+  
+  
 
 /*
 EX 2
@@ -75,7 +115,7 @@ db.inspections.aggregate([
 ])
 
 //Restaurantes con rating mayor a 4
-db.restaurants.find({rating : {$gt: 4}})
+db.restaurants.find({rating : {$gt: 4, $ne: 'Not yet rated'}})
 
 /*
 EX 3
@@ -197,4 +237,82 @@ db.restaurants.aggregate([
         }
     },
     { $out: "restaurants_inspections" } // guarda el resultado en una nueva collection
+])
+
+
+//Tareas avanzadas - NUEVA COLLECTION
+db.restaurants.aggregate([
+    {
+        $addFields: {
+            restaurant_id: { $toString: "$_id" }
+        }
+    },
+    {
+        "$lookup": {
+            "from": "inspections",
+            "localField": "restaurant_id",
+            "foreignField": "restaurant_id",
+            "as": "inspection_history"
+        }
+    },
+    {
+        $set: {
+            inspection_history: {
+                $map: {//Modificar estructura de inpection
+                    input: "$inspection_history",
+                    as: "insp",
+                    in: {
+                        id: "$$insp.id",
+                        certificate_number: "$$insp.certificate_number",
+                        business_name: "$$insp.business_name",
+                        date: {//Pasar a formato ISO
+                            $dateFromString: {
+                                dateString: "$$insp.date",
+                                format: "%b %d %Y"
+                            }
+                        },
+                        result: "$$insp.result",
+                        sector: "$$insp.sector"
+                    }
+                }
+            }
+        }
+    },
+    {
+        //Sacar del pipeline lo qeu no queremos
+        $unset: ["restaurant_id", "inspection_history.restaurant_id", "inspection_history.address"]
+    },
+    {
+        $addFields: {//Añadimos el rating con addFields
+            rating: {//Solo añadir si se trata de un valor númerico
+                $cond: {
+                    if: { $ne: [{ $type: "$rating" }, "string"] },
+                    then: "$rating",
+                    else: "$$REMOVE" //mongo 3.6 no muestra el campo -> remove
+                }
+            }
+        }
+    },
+    {
+        $out: "restaurants_with_inspections" //Creamos una nueva colección
+    }
+ ]);
+ 
+ //Ordenar inspeccions a partir de la fecha
+ db.restaurants_with_inspections.aggregate([
+    {
+      $match : { name : "2 Treat U"}
+    },
+    {
+        $unwind: "$inspection_history"
+    },
+    {
+        $sort: { "inspection_history.date": -1 }
+    },
+    {
+        $project: {
+            _id: 1,
+               inspection_history: 1
+        }
+    }
 ])
